02f4cda6be5dab9ecacc6e95147f47af04fd5b27
vinicius Puente <vinicius.puente@db1.com.br>
2020-09-14 21:59:16 -0300

    prepare marvel api integration

---
+++ src/services/marvel-api/marvel-api.service.ts
@@ -0,0 +1,46 @@
+
+import md5 from 'md5'
+import enviroment from '../../enviroment';
+
+export default class MarvelApiService {
+
+  private MARVEL_URI: string = 'https://gateway.marvel.com/v1/public/';
+
+  constructor() {
+  }
+
+  private get ts(){
+    return new Date().valueOf();
+  }
+
+  private get hash(): string {
+    return md5(`${this.ts}${enviroment.PRIVATE_KEY}${enviroment.PUBLIC_KEY}`)
+  }
+
+  private mountRequestString(apiPath:string): string {
+    return `${this.MARVEL_URI}/${apiPath}/ts=${this.ts}&apikey=${enviroment.PUBLIC_KEY}&hash=${this.hash}`;
+  }
+
+  private async mountRequest(apiPath:string, method: string): Promise<any> {
+    return fetch(apiPath, {
+      method: method, 
+    }) 
+  }
+
+  public async get(apiPath: string): Promise<any> {
+    return this.mountRequest(this.mountRequestString(apiPath), 'GET');
+  }
+
+  public async post(apiPath: string): Promise<any> {
+    return this.mountRequest(this.mountRequestString(apiPath), 'POST');
+  }
+
+  public async put(apiPath: string): Promise<any> {
+    return this.mountRequest(this.mountRequestString(apiPath), 'PUT');
+  }
+
+  public async delete(apiPath: string): Promise<any> {
+    return this.mountRequest(this.mountRequestString(apiPath), 'DELETE');
+  }
+
+}
